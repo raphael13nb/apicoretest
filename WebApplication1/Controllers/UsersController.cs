454ea33f15105606b2365267dbdb368b209dd656
Raphael bressam <raphaelwd@hotmail.com>
2018-09-09 15:35:44 -0300

    Add project files.

---
+++ WebApplication1/Controllers/UsersController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Data.User> Get()
+        {
+            IEnumerable<Data.User> result;
+            using (var dbContext = new Data.UserContext())
+            {
+                result = dbContext.Users.ToList();
+            }
+            return result;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Data.User Get(int id)
+        {
+            Data.User result;
+            using (var dbContext = new Data.UserContext())
+            {
+                result = dbContext.Users.Find(id);
+            }
+            return result;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public Data.User Post([FromBody]Data.User value)
+        {
+            using (var dbContext = new Data.UserContext())
+            {
+                dbContext.Users.Add(value);
+                dbContext.SaveChanges();
+            }
+
+            return value;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public Data.User Put(int id, [FromBody]Data.User value)
+        {
+            Data.User result;
+            using (var dbContext = new Data.UserContext())
+            {
+                result = dbContext.Users.Find(id);
+                if (result != null)
+                {
+                    result.firstName = value.firstName;
+                    result.lastName = value.lastName;
+                    result.updatedAt = DateTimeOffset.Now;
+                    dbContext.SaveChanges();
+                }
+            }
+            return result;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            using (var dbContext = new Data.UserContext())
+            {
+                var result = dbContext.Users.Find(id);
+                if (result != null)
+                {
+                    dbContext.Remove(result);
+                    dbContext.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ WebApplication1/Data/User.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Data
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=infinitvmlinuxdb.brazilsouth.cloudapp.azure.com;Database=ApiTest;User Id=sa;Password=!InfinitSolucoes11;Connection Timeout=36000;MultipleActiveResultSets=true;");
+        }
+    }
+
+    [Table("user")]
+    public class User
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public DateTimeOffset createdAt { get; set; }
+        public DateTimeOffset updatedAt { get; set; }
+    }
+}
